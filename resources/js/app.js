c6b9137a7d9e3288bdc07df65306c0a7da705a5b
José GUilherme <jose.guilherme96@outlook.com.br>
2024-09-21 10:32:51 -0300

    Importado vuetify, que será utlizado como base para construção de todo layout do frontend junto como o vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,18 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify/lib/framework.mjs';
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+
+    components,
+    directives
+
+})
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -15,6 +27,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(vuetify)
             .mount(el);
     },
     progress: {
