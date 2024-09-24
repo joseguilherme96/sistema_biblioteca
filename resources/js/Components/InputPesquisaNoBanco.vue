5ff8428e91a834392f95e05f2645417469338869
José GUilherme <jose.guilherme96@outlook.com.br>
2024-09-24 16:20:08 -0300

    Incluso componente que busca informações no banco

---
+++ resources/js/Components/InputPesquisaNoBanco.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <v-text-field v-model="form.valorPesquisado" label="Search" prepend-inner-icon="mdi-magnify" variant="outlined"
+        @input="buscarLivro" hide-details single-line
+        placeholder="Pesquise pelo nome do livro, autor ou ainda descrição"></v-text-field>
+
+</template>
+
+<script setup>
+
+import { useForm } from '@inertiajs/vue3';
+
+const props = defineProps({
+
+    route_dados: {
+
+        type: String
+
+    },
+
+})
+
+const form = useForm({
+
+    valorPesquisado:''
+
+});
+
+const submit = ()=>{
+
+    form.post(route(`${props.route_dados}`))
+
+}
+
+const buscarLivro = ()=>{
+
+    submit();
+
+}
+
+</script>
