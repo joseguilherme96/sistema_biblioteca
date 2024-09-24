734de56ee976d69fdc8b43c1ac7a3e39eb9caa28
José GUilherme <jose.guilherme96@outlook.com.br>
2024-09-24 16:23:03 -0300

    Incluso modal para editar os livros na listagem

---
+++ resources/js/Pages/Livro/Partials/ModalEditarLivro.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <v-container>
+        <v-row class="text-center">
+            <v-col cols="12" md="6">
+                <v-dialog v-model="modalVisible.exibir" max-width="900">
+                    <template v-slot:default="{ isActive }">
+                        <v-card>
+                            <Toolbar :data="{ title: modalVisible.title }"></Toolbar>
+                            <template v-slot:actions>
+                                <v-btn variant="flat"  color="#310740" class="ml-auto" text="Close" @click="modalVisible.exibir = false"></v-btn>
+                            </template>
+                        </v-card>
+                    </template>
+                </v-dialog>
+            </v-col>
+        </v-row>
+    </v-container>
+
+</template>
+<script setup>
+
+import Toolbar from '@/Components/Toolbar.vue';
+import {ref,watch} from 'vue'
+import FormLivro from './FormLivro.vue';
+
+const props = defineProps({
+
+    modalVisible: {
+
+        type: Object
+
+    }
+
+
+})
+
+const dialogVisible = ref(props.dialogVisible)
+
+watch(() => props.dialogVisible, (newvalue) => {
+
+    dialogVisible.value = newvalue;
+
+}, { deep: true });
+
+</script>
