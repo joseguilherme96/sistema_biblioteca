0e8eb7d0b003aaabaacbb9cf5e4bb2758cbc5216
José GUilherme <jose.guilherme96@outlook.com.br>
2024-10-12 11:19:21 -0300

    Criado botão padrão do sistema

---
+++ resources/js/Components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <v-btn variant="flat" color="#310740">{{ nome }}</v-btn>
+
+</template>
+<script setup>
+
+defineProps({
+
+    nome:{
+
+        type: String,
+        required:true
+
+    }
+
+
+})
+
+</script>
