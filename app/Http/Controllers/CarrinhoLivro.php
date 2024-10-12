c881b1e4b166b9a35f8725f23b34292415a35acf
José GUilherme <jose.guilherme96@outlook.com.br>
2024-10-12 16:28:47 -0300

    Atualizado nome pagina

--- app/Http/Controllers/CarrinhoLivro.php
+++ app/Http/Controllers/CarrinhoLivro.php
@@ -9,7 +9,7 @@ class CarrinhoLivro extends Controller
 {
     public function index(){
 
-        return Inertia::render('Carrinho/PageLivro', [
+        return Inertia::render('Carrinho/PageCarrinhoLivro', [
             'status' => session('status')
         ]);
     }
