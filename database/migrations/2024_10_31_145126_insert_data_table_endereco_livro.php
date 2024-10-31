5c9128eb516da2861c8d181efe70c11bfec5be40
José GUilherme <jose.guilherme96@outlook.com.br>
2024-10-31 14:52:17 -0300

    Adicionado migration para subir alguns cadastros de enderecos automaticos ao instalar o sistema

---
+++ database/migrations/2024_10_31_145126_insert_data_table_endereco_livro.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\EnderecoLivroModel;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('endereco_livro', function (Blueprint $table) {
+            //
+        });
+
+        $enderecoLivroModel = new EnderecoLivroModel();
+
+        $enderecoLivroModel->insert(
+
+            [
+                [
+                    'descricao' => 'Prateleira R1.P2.P.2'
+                ],
+                [
+                    'descricao' => 'Prateleira R1.P2.P.3'
+                ],
+                [
+                    'descricao' => 'Prateleira R1.P2.P.4'
+                ],
+                [
+                    'descricao' => 'Prateleira R1.P2.P.5'
+                ]
+            ]
+
+
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::delete('endereco_livro');
+    }
+};
