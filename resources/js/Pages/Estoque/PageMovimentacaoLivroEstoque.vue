37d758a1a5a77850f20ad2d25d7e2b6d3202b832
José GUilherme <jose.guilherme96@outlook.com.br>
2024-11-07 11:18:06 -0300

    Incluido cor nas quantidades movimentadas de acordo com a movimentação

--- resources/js/Pages/Estoque/PageMovimentacaoLivroEstoque.vue
+++ resources/js/Pages/Estoque/PageMovimentacaoLivroEstoque.vue
@@ -1,15 +1,16 @@
+
 <template>
 
     <AuthenticatedLayout>
         <h1 class="text-center" style="font-weight: 100; color: #310740">Movimentação Estoque</h1>
-        <v-data-table :items="movimentacoes"></v-data-table>
+        <v-data-table :items="movimentacoes" :fixed-header="true" :hover="true"></v-data-table>
     </AuthenticatedLayout>
 
 </template>
 <script setup>
 
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-
+import { onMounted } from 'vue';
 defineProps({
 
 
@@ -18,6 +19,28 @@ defineProps({
         type: Object,
         required: true
     }
+
+})
+
+
+onMounted(() => {
+
+    //Aplicando cor na quantidade, caso seja uma saida
+    document.querySelectorAll('.v-data-table__tr').forEach((linha) => {
+
+        let quantidade_movimentada = linha.children[5].innerHTML;
+
+        if (quantidade_movimentada < 0) {
+
+            linha.children[5].style.color = "red";
+
+        } else {
+
+            linha.children[5].style.color = "green";
+
+        }
+
+    })
 
 })
 
