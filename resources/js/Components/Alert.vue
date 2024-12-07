18d8b71484273074c30d7fa088e1067be1f6a227
José GUilherme <jose.guilherme96@outlook.com.br>
2024-12-07 13:28:09 -0300

    Conforme solicitado pela banca alterado caixas de mensagens para verde caso sucesso e vermelho caso erro

--- resources/js/Components/Alert.vue
+++ resources/js/Components/Alert.vue
@@ -7,18 +7,19 @@
 
                     <template v-slot:default="{ isActive }">
                         <v-card>
-                            <Toolbar :data="{ title: dialogVisible.title }"></Toolbar>
+                            <ToolbarSuccess :data="{ title: dialogVisible.title }" v-if="dialogVisible.status == 'success'"></ToolbarSuccess>
+                            <ToolbarError :data="{ title: dialogVisible.title }" v-if="dialogVisible.status == 'error'"></ToolbarError>
                             <v-row class="justify-center mt-5">
-                                <v-icon color="#310740" icon="mdi-alert" size="x-large"
+                                <v-icon color="#FF6347" icon="mdi-alert" size="x-large"
                                     v-show="dialogVisible.status == 'error'"></v-icon>
-                                <v-icon color="#310740" icon="mdi-check-circle" size="x-large"
+                                <v-icon color="#006400" icon="mdi-check-circle" size="x-large"
                                     v-show="dialogVisible.status == 'success'"></v-icon>
                             </v-row>
                             <v-card-text class="text-h6 pa-12 text-center">
                                 {{ dialogVisible.message }}
                             </v-card-text>
                             <template v-slot:actions>
-                                <v-btn variant="flat"  color="#310740" class="ml-auto" text="Fechar" @click="dialogVisible.exibir = false"></v-btn>
+                                <v-btn variant="flat"  :color="dialogVisible.status == 'success' ? '#006400' : '#FF6347'" class="ml-auto" text="Fechar" @click="dialogVisible.exibir = false"></v-btn>
                             </template>
                         </v-card>
                     </template>
@@ -32,6 +33,8 @@
 
 import {ref,watch} from 'vue'
 import Toolbar from './Toolbar.vue';
+import ToolbarSuccess from './ToolbarSuccess.vue';
+import ToolbarError from './ToolbarError.vue';
 
 const props = defineProps({
 
---
+++ resources/js/Components/ToolbarSuccess.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <v-toolbar :title="content.title" style="background-color: #006400; color: white;"></v-toolbar>
+
+</template>
+
+<script setup>
+
+    const props = defineProps({
+
+        data:{
+
+            type:Object,
+            required:true
+
+        }
+
+    })
+
+    const content = props.data;
+
+</script>
