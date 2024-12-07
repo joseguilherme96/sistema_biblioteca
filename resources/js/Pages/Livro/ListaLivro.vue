c57eefe896aeb2393d6566bd4c57eaac7a684423
José GUilherme <jose.guilherme96@outlook.com.br>
2024-12-07 16:07:20 -0300

    Conforme solicitado pela banca, incluso menu suspenso para eliminar excesso de botões para acesso master

--- resources/js/Pages/Livro/ListaLivro.vue
+++ resources/js/Pages/Livro/ListaLivro.vue
@@ -1,5 +1,5 @@
 <template>
-    <AuthenticatedLayout :auth="auth" @permissoes="(value)=>setPermissoes(value)">
+    <AuthenticatedLayout :auth="auth" @permissoes="(value) => setPermissoes(value)">
         <ModalEditarLivro :modalVisible="modal"></ModalEditarLivro>
         <Alert :dialogVisible="mensagem"></Alert>
         <v-sheet class="mx-auto m-5" width="900">
@@ -7,13 +7,29 @@
             <v-row class="justify-content-center">
                 <v-col cols="12" md="4" lg="3" v-for="(livro, index) in livros" class="text-center">
                     <CardLivro :livro="livro">
-                        <v-btn variant="flat" color="#310740" class="m-1" @click="reservar(livro)"
-                            v-show="livro.qtd_lvro_estqe > 0">Reservar</v-btn>
-                        <v-chip color="red" text-color="white" v-show="livro.qtd_lvro_estqe == 0"
-                            rounded="1">Indisponível</v-chip>
-                        <v-btn variant="flat" color="#310740" class="m-1"
-                            @click="abrirModalEditarLivro(livro)" v-show="permissoes.livros.editar">Editar</v-btn>
-                        <v-btn variant="flat" color="#310740" class="m-1" @click="deletarLivro" v-show="permissoes.livros.deletar">Deletar</v-btn>
+                        <div class="col-12">
+                            <div class="row justify-content-center">
+                                <div class="col-6 p-2">
+                                    <v-btn variant="flat" color="#310740" @click="reservar(livro)"
+                                        v-show="livro.qtd_lvro_estqe > 0" class="ml-5">Reservar</v-btn>
+                                    <v-chip color="red" text-color="white" v-show="livro.qtd_lvro_estqe == 0"
+                                        rounded="1" class="ml-5">Indisponível</v-chip>
+                                </div>
+                                <div v-if="permissoes.livros.editar && permissoes.livros.deletar"
+                                    class="d-inline col-2 mr-5">
+                                    <v-icon left small color="#310740" @click="livro.exibirBotoesExtras = true"
+                                        v-show="livro.exibirBotoesExtras != true">mdi-chevron-down</v-icon>
+                                    <v-icon left small color="#310740" @click="livro.exibirBotoesExtras = false"
+                                        v-show="livro.exibirBotoesExtras == true">mdi-chevron-up</v-icon>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-block m-1">
+                            <v-btn variant="flat" color="#310740" class="m-1" @click="abrirModalEditarLivro(livro)"
+                                v-show="permissoes.livros.editar && livro.exibirBotoesExtras">Editar</v-btn>
+                            <v-btn variant="flat" color="#310740" class="m-1" @click="deletarLivro"
+                                v-show="permissoes.livros.deletar && livro.exibirBotoesExtras">Deletar</v-btn>
+                        </div>
                     </CardLivro>
                 </v-col>
             </v-row>
@@ -31,14 +47,14 @@ import Alert from '@/Components/Alert.vue';
 
 const modal = ref({ data: null, exibir: false, title: 'Editar', livro: null });
 const livrosCarrinho = ref([])
-const mensagem = ref({message:'O livro foi adicionado ao carrinho com sucesso !',title:'Mensagem',exibir:false})
+const mensagem = ref({ message: 'O livro foi adicionado ao carrinho com sucesso !', title: 'Mensagem', exibir: false })
 
 defineProps({
 
     livros: {
 
         type: Object
-        
+
     },
     auth: {
 
@@ -82,7 +98,7 @@ onMounted(() => {
 
 })
 
-const setPermissoes = (value)=>{
+const setPermissoes = (value) => {
 
     permissoes.value = value;
 
