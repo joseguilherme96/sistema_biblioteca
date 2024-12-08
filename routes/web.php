5f0c16bcc3b329bb5ec94a87e00157e86b7bdfc2
José GUilherme <jose.guilherme96@outlook.com.br>
2024-12-08 09:18:35 -0300

    Adicionado verificação de usuario tipo 1 na rota profile

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth','verificar-usuario-tipo-1'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
